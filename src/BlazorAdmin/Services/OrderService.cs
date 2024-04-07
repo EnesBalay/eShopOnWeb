d447857ce263db49d0f87d48b65a09300e87c98a
Enes Balay <enesbalay@outlook.com>
2024-04-07 06:57:46 +0300

    added: Added order services to Blazor Admin

---
+++ src/BlazorAdmin/Services/OrderService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorShared.Interfaces;
+using BlazorShared.Models;
+using Microsoft.Extensions.Logging;
+
+
+namespace BlazorAdmin.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly HttpService _httpService;
+    private readonly ILogger<OrderService> _logger;
+
+    public OrderService(HttpService httpService, ILogger<OrderService> logger)
+    {
+        _httpService = httpService;
+        _logger = logger;
+    }
+
+    public async Task<Order> ChangeStatus(Order order)
+    {
+        var orderTask = _httpService.HttpPut<UpdateOrderResponse>($"order-status", order);
+        await Task.WhenAll(orderTask);
+        var newOrder = orderTask.Result.Order;
+        return newOrder;
+    }
+
+    public async Task<OrderDetail> GetById(int id)
+    {
+        var orderTask = _httpService.HttpGet<DetailOrderResponse>($"orders/" + id);
+        await Task.WhenAll(orderTask);
+        var order = orderTask.Result.Order;
+        return order;
+    }
+
+    public async Task<List<Order>> List()
+    {
+        _logger.LogInformation("Fetching orders from API.");
+
+        var orderListTask = _httpService.HttpGet<ListOrderResponse>($"orders");
+        await Task.WhenAll(orderListTask);
+        var items = orderListTask.Result.Orders;
+        return items;
+    }
+}
---
+++ src/BlazorShared/Interfaces/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorShared.Models;
+
+namespace BlazorShared.Interfaces;
+public interface IOrderService
+{
+    Task<Order> ChangeStatus(Order order);
+    Task<OrderDetail> GetById(int id);
+    Task<List<Order>> List();
+}
