18a97a881b63e856e3f692158e7447ac2c9eca45
Enes Balay <enesbalay@outlook.com>
2024-04-07 07:09:17 +0300

    updated: User panel updated according to new order status model

--- src/Web/ViewModels/OrderViewModel.cs
+++ src/Web/ViewModels/OrderViewModel.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using static BlazorShared.Models.Order;
+
 
 namespace Microsoft.eShopWeb.Web.ViewModels;
 
 public class OrderViewModel
 {
-    private const string DEFAULT_STATUS = "Pending";
-
+    private const int DEFAULT_STATUS = 0;
+    public OrderViewModel()
+    {
+        Status = DEFAULT_STATUS;
+    }
     public int OrderNumber { get; set; }
     public DateTimeOffset OrderDate { get; set; }
     public decimal Total { get; set; }
-    public string Status => DEFAULT_STATUS;
+    public OrderStatus Status { get ; set; }
     public Address? ShippingAddress { get; set; }
 }
